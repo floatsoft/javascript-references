eaa869809efb4002690861c6c9dca9edebba2fcb
Kevin Gonzalez <typekev@gmail.com>
2019-06-03 12:27:57 +0200

    Hello world examples with documentation for es5 and es6 with import and export

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javascript-references
 Material to reference when working with JavaScript 
+
+# Hello World
+This reference explains how to print "Hello World" to the console with JavaScript.
---
+++ index.js
@@ -0,0 +1,18 @@
+/** No need to "use strict"
+    In this case even though we are not declaring this code as an ES6 module
+    the following code contains no syntax that can be caught by strict mode.
+    See ./varDeclaration.js for an example that can benefit from strict mode.
+*/
+
+/** The Console object provides access to the browser's debugging console
+    See https://developer.mozilla.org/en-US/docs/Web/API/console
+    The Console object's methods include log()
+    The Console method log() outputs a message to the console.
+    The message is not limited to a single string. 
+    See https://developer.mozilla.org/en-US/docs/Web/API/Console/log
+*/
+console.log("Hello World");
+
+/** The easiest way to test this file is to run the following line in your console:
+    node index.js
+*/
