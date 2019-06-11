4ede5796253ab718692996cca29de847ae660965
Kevin Gonzalez <typekev@gmail.com>
2019-06-11 17:52:28 +0200

    Added documentation with references for quicksort func declaration and return

---
+++ quicksort/index.js
@@ -0,0 +1,22 @@
+/**
+ * This is an implementation of the Quicksort algorithm written in JavaScript.
+ * See https://en.wikipedia.org/wiki/Quicksort#Algorithm
+ */
+
+/**
+ * The following code contains syntax that, if written incorrectly, would be caught by strict mode.
+ * See https://github.com/floatsoft/javascript-references/blob/hello-world/varDeclaration.js#L1-L7
+ */
+"use strict";
+
+/**
+ * We declare a function named quicksort with a single parameter; arr.
+ * See https://github.com/floatsoft/javascript-references/blob/bubble-sort/bubble-sort/index.js#L13-L19
+ */
+function quicksort(arr) {
+  /**
+   * We return our sorted array, arr.
+   * See https://github.com/floatsoft/javascript-references/blob/bubble-sort/bubble-sort/index.js#L171-L176
+   */
+  return arr;
+}
