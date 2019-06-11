38aeae69b9cad841735fa73750d1f9df2ccddacc
Kevin Gonzalez <typekev@gmail.com>
2019-06-11 17:53:20 +0200

    Added documentation with references for array declaration

--- quicksort/index.js
+++ quicksort/index.js
@@ -20,3 +20,10 @@ function quicksort(arr) {
    */
   return arr;
 }
+
+/**
+ * We declare an array of unsorted numbers using the array literal method,
+ * we name our new variable unsortedNumbersList.
+ * See https://github.com/floatsoft/javascript-references/blob/bubble-sort/bubble-sort/index.js#L179-L193
+ */
+var unsortedNumbersList = [8, 5, 6, 9, 3, 1, 4, 2, 7, 10];
